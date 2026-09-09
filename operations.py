261e12cb34b9b59b322ad815d94a32a6d2ecbea6
mahimfatima <amahinfatima55@gmail.com>
2026-09-09 10:16:11 +0530

    Implement basic arithmetic operations on two numbers
    The program reads two numbers and prints their sum, difference, product, quotient, and remainder.

---
+++ operations.py
@@ -0,0 +1,8 @@
+#read two numbers and print their sum,difference,product,quotient and remainder
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+print("sum:",a+b)
+print("difference:",a-b)
+print("product:",a*b)
+print("quorient:",a/b)
+print("remainder:",a%b)
